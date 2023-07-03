f69e420471b695eadc514039ef88d4e14ef3210c
Abe <ai_980@inbox.ru>
2023-07-03 18:44:49 +0300

    hw2comm

--- HW_2/src/Interfaces/iReturnOrder.java
+++ HW_2/src/Interfaces/iReturnOrder.java
@@ -2,6 +2,9 @@ package Interfaces;
 
 import Classes.Actor;
 
+/*
+ * пришлось сделать родителем iActorBehaviuor, чтобы не переписывать весь Market.java
+ */
 public interface iReturnOrder {
     
     public boolean isReturnOrder() ;  // возвращает хочет ли посетитель вернуть заказ
